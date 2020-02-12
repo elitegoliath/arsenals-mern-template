91150fed1f85ce7f48bfc93f9d2eaeaa250e1a0a
Arsenal Lagspike <elitegoliath@hotmail.com>
2020-02-12 14:52:15 -0600

    Added more to the server. Still figuring out how I want this to look.

--- server/src/routes/samples/samples.route.ts
+++ server/src/routes/samples/samples.route.ts
@@ -1,10 +1,23 @@
 import { Router, Request, Response } from 'express'
 import { Sample } from '../../models'
+import { composeApiFunc } from '../app.router'
 
 const SamplesRouter = Router()
 
-SamplesRouter.route('/').get((req: Request, res: Response) => {
-  Sample.find
-})
+SamplesRouter.route('/').get(composeApiFunc(async (req: Request, res: Response) => {
+  const foundSample = await Sample.find()
+  return res.json(foundSample)
+}))
+
+SamplesRouter.route('/add').post(composeApiFunc(async (req: Request, res: Response) => {
+  const newSample = new Sample({
+    sampleName: req.body.sampleName,
+    sampleValue: req.body.sampleValue,
+  })
+
+  await newSample.save()
+  
+  return res.json('New Sample has been created. Good work, lad!')
+}))
 
 export default SamplesRouter
