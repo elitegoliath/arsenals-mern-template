53831338438c6e205156a6d200f71e4fc0b606fa
Arsenal Lagspike <elitegoliath@hotmail.com>
2020-02-14 09:31:45 -0600

    Began setting up two database routes for different ways to authenticate users.

--- server/src/routes/user/user.route.ts
+++ server/src/routes/user/user.route.ts
@@ -1,6 +1,27 @@
 import { Router, Request, Response } from 'express'
 import { composeApiFunc } from '../routerUtils'
+import { authenticate } from 'passport'
 
 const UserRouter = Router()
+
+UserRouter.route('/proxy/login').post(authenticate('local-proxy'), composeApiFunc(async (_req: Request, _res: Response) => {
+  // Redirect...
+}))
+
+UserRouter.route('/login').post(authenticate('local'), composeApiFunc(async (_req: Request, _res: Response) => {
+  // Redirect
+}))
+
+UserRouter.route('/proxy/register').post(composeApiFunc(async (_req: Request, _res: Response) => {
+  // Register
+  // Log in
+  // Redirect
+}))
+
+UserRouter.route('/register').post(composeApiFunc(async (_req: Request, _res: Response) => {
+  // Register
+  // Log in
+  // Redirect
+}))
 
 export default UserRouter
